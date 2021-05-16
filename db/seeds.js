7710eeea7ce79897bb62b792582a899e8fd57739
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-05-15 20:47:32 -0500

    updated db files
    work on the schema and seeds.js file

---
+++ db/seeds.js
@@ -0,0 +1,44 @@
+const User = require('../models/User');
+const sequelize = require('../config/connection');
+
+const userData = [
+    {
+        username: 'the_silent_wolf',
+        email: 'ned@winterfell.com',
+        password: 'winteriscoming',
+    },
+    {
+        username: 'king_slayer',
+        email: 'jaime@casterlyrock.com',
+        password: 'brienne',
+    },
+    {
+        username: 'stormborn',
+        email: 'daenerys@targaryen.com',
+        password: 'wherearemydragons',
+    },
+    {
+        username: 'hodor',
+        email: 'hodor@hodor.com',
+        password: 'hodorhodor',
+    },
+    {
+        username: 'snowbro',
+        email: 'jon@thewall.com',
+        password: 'ghost'
+    }
+];
+
+sequelize
+    .sync({ force: true })
+    .then(() => {
+        return User.bulkCreate(userData);
+    })
+    .then(data => {
+        console.log('\n Users seeded!');
+        process.exit(0);
+    })
+    .catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
